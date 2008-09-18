3eedd57cbda25b0282f8a0e287d532da43d0496b
Ben Askins <benj@silversun.local>
2008-09-18 16:16:13 +1000

    A few tweaks.

--- exercises/xhtml1/scripts/exercises/two.js
+++ exercises/xhtml1/scripts/exercises/two.js
@@ -1,20 +1,34 @@
+Element.addMethods({
+  morph_chain: function(element, states) {
+    states.each( function(state) {
+      $(element).morph(state, {queue: 'end'});
+    })
+  }
+})
+
 Event.addBehavior({
   '#button-colour:click': function(event) {
-    $('changeme').morph({color:'#ff0000'}, { queue: 'end' });
-    $('changeme').morph({color:'#00ff00'}, { queue: 'end' });
-    $('changeme').morph({color:'#0000ff'}, { queue: 'end' });
+    $('changeme').morph_chain([
+      {color:'#ff0000'}, 
+      {color:'#00ff00'}, 
+      {color:'#0000ff'}
+    ]);
     return false;
   },
   '#button-size:click': function(event) {
-    $('changeme').morph({fontSize:'4.0em'}, { queue: 'end' });
-    $('changeme').morph({fontSize:'1.0em'}, { queue: 'end' });
+    $('changeme').morph_chain([
+      {fontSize:'4.0em'}, 
+      {fontSize:'1.0em'}
+    ]);
     return false;
   },
   '#button-both:click': function(event) {
-    $('changeme').morph({fontSize:'4.0em', color:'#ff0000'}, { queue: 'end' });
-    $('changeme').morph({fontSize:'1.0em', color:'#00ff00'}, { queue: 'end' });
-    $('changeme').morph({fontSize:'4.0em', color:'#0000ff'}, { queue: 'end' });
-    $('changeme').morph({fontSize:'1.0em', color:'#000000'}, { queue: 'end' });
+    $('changeme').morph_chain([
+      {fontSize:'4.0em', color:'#ff0000'},
+      {fontSize:'1.0em', color:'#00ff00'},
+      {fontSize:'4.0em', color:'#0000ff'},
+      {fontSize:'1.0em', color:'#000000'}
+    ]);
     return false;
   }  
 });
