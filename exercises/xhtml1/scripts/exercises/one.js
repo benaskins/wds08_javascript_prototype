69ff94ab5bb75045ddac32b91bb1f45f687f20a9
Ben Askins <benj@silversun.local>
2008-09-19 16:26:18 +1000

    A few more tweaks.

--- exercises/xhtml1/scripts/exercises/one.js
+++ exercises/xhtml1/scripts/exercises/one.js
@@ -1,10 +1,8 @@
 Event.addBehavior({
-  '#easel > div:mouseover': function(event) {
-    if ($('rick') == null) {
-      this.update("<img id='rick' src='../graphics/scriptaculous/mouseover.jpg'");      
-    }
+  '#easel img:mouseover': function(event) {
+    this.writeAttribute('src', '../graphics/scriptaculous/mouseover.jpg');      
   },
-  '#easel > div:mouseout': function(event) {
-    this.update("<img src='../graphics/clear.gif'");
+  '#easel img:mouseout': function(event) {
+    this.writeAttribute('src', '../graphics/clear.gif');
   }  
 });
